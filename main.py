e699f97cdb975277c1006d4e4fa92358f076eebd
AlthariqFairuz <althariqfairuz273@gmail.com>
2023-11-19 13:52:42 +0700

    fix : fix minor bugs in main (rule is unfinished)

--- main.py
+++ main.py
@@ -1,26 +1,58 @@
-import argparse
+from PDA import PDA
+from Tokenizer import Tokenizer as tokenize
+from TagChecker import TagChecker as tagcheck
 
-# Load arguments with argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("pda_filename", help="PDA filename")
-parser.add_argument("html_filename", help="HTML filename")
-args = parser.parse_args()
+def valid ():
+    print("""
+    ██╗░░░██╗░█████╗░██╗░░░░░██╗██████╗░  ██╗░░██╗████████╗███╗░░░███╗██╗░░░░░
+    ██║░░░██║██╔══██╗██║░░░░░██║██╔══██╗  ██║░░██║╚══██╔══╝████╗░████║██║░░░░░
+    ╚██╗░██╔╝███████║██║░░░░░██║██║░░██║  ███████║░░░██║░░░██╔████╔██║██║░░░░░
+    ░╚████╔╝░██╔══██║██║░░░░░██║██║░░██║  ██╔══██║░░░██║░░░██║╚██╔╝██║██║░░░░░
+    ░░╚██╔╝░░██║░░██║███████╗██║██████╔╝  ██║░░██║░░░██║░░░██║░╚═╝░██║███████╗
+    ░░░╚═╝░░░╚═╝░░╚═╝╚══════╝╚═╝╚═════╝░  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░░░░╚═╝╚══════╝""")
 
-# Get arguments
-pda_filename = args.pda_filename
-html_filename = args.html_filename
+def invalid () :    
+    print("""
+    ░██████╗██╗░░░██╗███╗░░██╗████████╗░█████╗░██╗░░██╗  ███████╗██████╗░██████╗░░█████╗░██████╗░
+    ██╔════╝╚██╗░██╔╝████╗░██║╚══██╔══╝██╔══██╗╚██╗██╔╝  ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗
+    ╚█████╗░░╚████╔╝░██╔██╗██║░░░██║░░░███████║░╚███╔╝░  █████╗░░██████╔╝██████╔╝██║░░██║██████╔╝
+    ░╚═══██╗░░╚██╔╝░░██║╚████║░░░██║░░░██╔══██║░██╔██╗░  ██╔══╝░░██╔══██╗██╔══██╗██║░░██║██╔══██╗
+    ██████╔╝░░░██║░░░██║░╚███║░░░██║░░░██║░░██║██╔╝╚██╗  ███████╗██║░░██║██║░░██║╚█████╔╝██║░░██║
+    ╚═════╝░░░░╚═╝░░░╚═╝░░╚══╝░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝""")
 
-# Load PDA from file
+# Fungsi untuk membaca file HTML
+def load_html(filename):
+    with open(filename, 'r') as file:
+        html = file.read()
+    return html
 
+config_file = input("Masukkan nama file konfigurasi: ")
 
-# Load html from file
+# Parse the configuration file
+if not PDA.parse_file(config_file):
+    print("Gagal membaca file konfigurasi.")
+    exit(1)
 
+# Ask the user for the word to check
+html_file = input("Masukkan file html yang akan di cek: ")
+html = load_html(html_file)
+tokens = tokenize.tokenizer(html)
+for token in tokens:
+    if tagcheck.tag_checker(token) == False:
+        invalid()
+        print(f"Invalid Token: {token}")
+        exit(1)
 
-# Parse
-isAccepted = True
+# Check if the first and last token is <html> and </html>
+if tokens[0] != "<html>" or tokens[len(tokens)-1] != "</html>":
+    invalid()
+    exit(1)
 
-# Print result
-if isAccepted:
-    print("Accepted")
+# Generate the automaton with the given start symbol, input word, and start stack symbol
+result = PDA.generate(PDA.start_symbol, tokens, PDA.start_stack, [(PDA.start_symbol, tokens, PDA.start_stack)])
+
+if result == 1:
+    valid()
 else:
-    print("Syntax Error")
+    invalid()
+
--- main2.py
+++ main2.py
@@ -1,27 +1,26 @@
-from PDA import PDA
+import argparse
 
-# Fungsi untuk membaca file HTML
-def load_html(filename):
-    with open(filename, 'r') as file:
-        html = file.read()
-    return html
+# Load arguments with argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("pda_filename", help="PDA filename")
+parser.add_argument("html_filename", help="HTML filename")
+args = parser.parse_args()
 
-config_file = input("Masukkan nama file konfigurasi: ")
+# Get arguments
+pda_filename = args.pda_filename
+html_filename = args.html_filename
 
-# Parse the configuration file
-if not PDA.parse_file(config_file):
-    print("Gagal membaca file konfigurasi.")
-    exit(1)
+# Load PDA from file
 
-# Ask the user for the word to check
-html_file = input("Masukkan file html yang akan di cek: ")
-html = load_html(html_file)
 
-# Generate the automaton with the given start symbol, input word, and start stack symbol
-result = PDA.generate(PDA.start_symbol, html, PDA.start_stack, [(PDA.start_symbol, html, PDA.start_stack)])
+# Load html from file
 
-if result == 1:
+
+# Parse
+isAccepted = True
+
+# Print result
+if isAccepted:
     print("Accepted")
-
 else:
     print("Syntax Error")
