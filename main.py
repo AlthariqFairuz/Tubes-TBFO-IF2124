2374bf562e27369f9290b6c03e3513d726261782
AlthariqFairuz <althariqfairuz273@gmail.com>
2023-11-26 11:20:45 +0700

    feat: add argparse

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 import argparse
 from PDA import PDA
 from Tokenizer import Tokenizer as tokenize
-import argparse
-
-# Load arguments with argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("pda_filename", help="PDA filename")
-parser.add_argument("html_filename", help="HTML filename")
-args = parser.parse_args()
-
-# Get arguments
-pda_filename = args.pda_filename
-html_filename = args.html_filename
 
 def valid():
     print("""
@@ -33,20 +22,28 @@ def invalid():
 
 # Fungsi untuk membaca file HTML
 def load_html(filename):
-    with open(filename, 'r') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         html = file.read()
     return html
 
-# Parse the configuration file
-if not PDA.file_parser(pda_filename):
-    print("Gagal membaca file konfigurasi.")
+
+# Bikin parser
+parser = argparse.ArgumentParser()
+
+parser.add_argument("ConfigFile", metavar="config", type=str, help="File Rules PDA")
+parser.add_argument("HtmlFile", metavar="html", type=str, help="HTML yang akan dicek")
+
+args = parser.parse_args()
+
+# Parse rules PDA
+if not PDA.read_rules(args.ConfigFile):
+    print("Gagal membaca file.txt.")
     exit(1)
 
-# Tokenize html file
-html = load_html(html_filename)
+html = load_html(args.HtmlFile)
 tokens = tokenize.tokenizer(html)
 
-# Generate automata dengan start symbol, input word, dan start stack symbol yang ada.
+# Generate autoamta dengan start symbol, input word, dan start stack symbol yang ada
 result = PDA.generate(PDA.start_symbol, "".join(tokens), PDA.start_stack, [(PDA.start_symbol, "".join(tokens), PDA.start_stack)])
 
 if result == 1:
